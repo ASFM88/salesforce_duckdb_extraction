0a5116e961f94392de9af8a38d7e92818da2ea3d
Andre Machado Eurochem <andre.machado@eurochemsam.com>
2025-05-21 15:33:09 -0300

    Adicionados os novos objetos ao STAGE

--- main.py
+++ main.py
@@ -12,9 +12,27 @@ conn_stage = duckdb.connect("db/stage_salesforce.duckdb")
 
 # Lista de tabelas RAW para processar
 objetos_raw = {
+    "raw_recordtype": "stage_recordtype",
+    "raw_user": "stage_user",
     "raw_account": "stage_account",
     "raw_contact": "stage_contact",
-    "raw_order": "stage_order"
+    "raw_cultura": "stage_cultura",
+    "raw_endereco": "stage_endereco",
+    "raw_cidade": "stage_cidade",
+    "raw_order": "stage_order",
+    "raw_orderitem": "stage_orderitem",
+    "raw_vendedores": "stage_vendedores",
+    "raw_tipo_comissao": "stage_tabela_de_comissao",
+    "raw_tipo_vencimento": "stage_tipo_negociacao",
+    "raw_unidade": "stage_unidade",
+    "raw_vencimento_contrato": "stage_vencimento_pedido",
+    "raw_pagamento": "stage_pagamento",
+    "raw_product": "stage_product2",
+    "raw_pricebook": "stage_pricebook2",
+    "raw_top": "stage_top",
+    "raw_previsao_entrega": "stage_previsao_entrega_produto",
+    "raw_cliente_final": "stage_clientefinalentrega",
+    "raw_frete": "stage_frete"
 }
 
 # Loop de transformação básica
@@ -38,4 +56,5 @@ conn_raw.close()
 conn_stage.close()
 
 print("✅ Transformação para camada STAGE concluída.")
+
 # %%
